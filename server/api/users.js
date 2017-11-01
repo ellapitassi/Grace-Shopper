d80841230b6fbf81524dbbf12887e26a2a25c882
Annelise Thorsen <annelise.thorsen@gmail.com>
2017-11-01 15:35:16 -0400

    all users route works

--- server/api/users.js
+++ server/api/users.js
@@ -3,28 +3,29 @@ const { User, Teachables } = require('../db/models');
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  console.log("HEREEEEE")
   User.findAll({ include: [{ model: Teachables }] })
     .then(users => res.json(users))
     .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-  User.findById(req.params.id)
-    .then(user => res.json(user))
-    .catch(next);
-})
-
 //Get All users by a given Teachable
-router.get('/:teachable/', (req, res, next) => {
+router.get('/teachable/:teachable/', (req, res, next) => {
   const teachable = req.params.teachable; //Assuming this is an id number? Also works if stored as string?
+  console.log(teachable)
   User.findAll({ include: [{ model: Teachables }] })
     .then(users => {
       var filteredUsers = users.filter(user => 
-        (user.getTeachables().indexOf(teachable) != -1) // teachables: [5, 3, 88], so indexOf(3) != -1
+        console.log(user.getTeachables())
+        //(user.getTeachables().indexOf(teachable) != -1) // teachables: [5, 3, 88], so indexOf(3) != -1
       );
       res.json(filteredUsers);
     })
+    .catch(next);
+}) 
+
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
     .catch(next);
 })
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,25 +2,11 @@ const User = require('./user')
 const Teachables = require('./teachables')
 const Transactions = require('./transactions')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
-
 //Associations
 Teachables.belongsToMany(User, {through: 'userTeachables'} )
 User.belongsToMany(Teachables, {through: 'userTeachables'} )
-Transactions.hasOne(User, {as: 'buyer'} )
-Transactions.hasOne(User, {as: 'tutor'} )
+Transactions.belongsTo(User, {as: 'buyer'} )
+Transactions.belongsTo(User, {as: 'tutor'} )
 Transactions.hasOne(Teachables)
 
 module.exports = {
@@ -28,4 +14,3 @@ module.exports = {
   Teachables,
   Transactions
 }
-
