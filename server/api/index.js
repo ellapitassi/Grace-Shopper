d51b4c761b13690d24fb5c17e62db3eb178d018f
jenniferneale <jeneale6@gmail.com>
2017-11-02 17:57:24 -0400

    Added router.use for cart to api index

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/cart', require('./cart'))
 router.use('/users', require('./users'))
 router.use('/transactions', require('./transactions'))
 router.use('/teachables', require('./teachables'))
 router.use('/orders', require('./orders'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
