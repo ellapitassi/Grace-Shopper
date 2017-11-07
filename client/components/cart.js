4d114dcfc64fbb25fbe4ddff6b4f8e8d69da8b57
jenniferneale <jeneale6@gmail.com>
2017-11-07 14:35:53 -0500

    Added routes and links for Shopping cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { fetchOrder } from '../store';
+import { Link } from 'react-router-dom'
 
 export class Cart extends Component {
 
@@ -27,63 +28,65 @@ export class Cart extends Component {
     return (
       <div>
         <h3>Shopping Cart</h3>
-        <form onSubmit={handleSubmit} name={name}>
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Tutor</th>
-                <th>Teachable</th>
-                <th>Date/Time</th>
-                <th>Duration</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {/*The cart is an Order object, so each lineItem is a Transaction object.*/}
-              {cart.transactions && Array.isArray(cart.transactions) && cart.transactions.length>0 && cart.transactions.map(lineItem => (
-                <tr key={lineItem.id}>
-                  <td><img src={lineItem.tutor.img} className="lineItemImg" /></td>
-                  <td>{lineItem.tutor.name}</td>
-                  <td>{lineItem.teachable.name} ${lineItem.teachable.price}/hr</td>
-                  <td>{this.dateFormat(lineItem.sessionTime)}</td>
-                  <td>
-                    <div>
-                      <button type="button" className="duration-adjust duration-minus" handleClick={this.props.handleMinusClick}>-</button>
-                      <input type="text" ref={"duration-select-display-"+lineItem.id} className="duration-select-display" size='2' pattern="[0-9]*"
-                        defaultValue={lineItem.duration ? lineItem.duration : 0} min="0" />
-                      <button type="button" className="duration-adjust duration-plus" handleClick={this.props.handlePlusClick}>+</button>
-                    </div>
-                  </td>
-                  <td className="cart-row-total">${lineItem.cost}</td>
-                  <td> <button type="button" className="remove-from-cart" handleClick={this.props.handleDeleteClick}>X</button> </td>
+        {(cart.transactions && Array.isArray(cart.transactions) && cart.transactions.length > 0) ?
+          <form onSubmit={handleSubmit} name={name}>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Tutor</th>
+                  <th>Teachable</th>
+                  <th>Date/Time</th>
+                  <th>Duration</th>
+                  <th></th>
                 </tr>
-              ))}
-              <tr>
-                <td></td><td></td><td></td><td></td>
-                <td>Subtotal</td>
-                <td className="cart-subtotal" id="cart-subtotal">
-                  {cart.subtotal}</td>
-              </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td>
-                <td>Taxes</td>
-                <td id="cart-taxes">${cart.taxes}</td>
-              </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td>
-                <td>Shipping</td>
-                <td>${cart.shippingCost}</td>
-              </tr>
-              <tr>
-                <td></td><td></td><td></td><td></td>
-                <td>Grand Total</td>
-                <td name="grandTotal">${cart.grandTotal}</td>
-              </tr>
+              </thead>
+              <tbody>
+                {/*The cart is an Order object, so each lineItem is a Transaction object.*/}
+                {cart.transactions.map(lineItem => (
+                  <tr key={lineItem.id}>
+                    <td><img src={lineItem.tutor.img} className="lineItemImg" /></td>
+                    <td>{lineItem.tutor.name}</td>
+                    <td>{lineItem.teachable.name} ${lineItem.teachable.price}/hr</td>
+                    <td>{this.dateFormat(lineItem.sessionTime)}</td>
+                    <td>
+                      <div>
+                        <button type="button" className="duration-adjust duration-minus" handleClick={this.props.handleMinusClick}>-</button>
+                        <input type="text" ref={"duration-select-display-" + lineItem.id} className="duration-select-display" size='2' pattern="[0-9]*"
+                          defaultValue={lineItem.duration ? lineItem.duration : 0} min="0" />
+                        <button type="button" className="duration-adjust duration-plus" handleClick={this.props.handlePlusClick}>+</button>
+                      </div>
+                    </td>
+                    <td className="cart-row-total">${lineItem.cost}</td>
+                    <td> <button type="button" className="remove-from-cart" handleClick={this.props.handleDeleteClick}>X</button> </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td></td><td></td><td></td><td></td>
+                  <td>Subtotal</td>
+                  <td className="cart-subtotal" id="cart-subtotal">
+                    {cart.subtotal}</td>
+                </tr>
+                <tr>
+                  <td></td><td></td><td></td><td></td>
+                  <td>Taxes</td>
+                  <td id="cart-taxes">${cart.taxes}</td>
+                </tr>
+                <tr>
+                  <td></td><td></td><td></td><td></td>
+                  <td>Shipping</td>
+                  <td>${cart.shippingCost}</td>
+                </tr>
+                <tr>
+                  <td></td><td></td><td></td><td></td>
+                  <td>Grand Total</td>
+                  <td name="grandTotal">${cart.grandTotal}</td>
+                </tr>
               </tbody>
-          </table>
-          {/*error && error.response && <div> {error.response.data} </div>*/}
-        </form>
+            </table>
+            {/*error && error.response && <div> {error.response.data} </div>*/}
+          </form>
+          : <p>Your cart is empty! Why not go look at available <Link to="/tutors">Tutors</Link>?</p>}
       </div>
     )
   }
