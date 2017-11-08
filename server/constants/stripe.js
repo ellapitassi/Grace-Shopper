7572b49e57aa229aa87a38e3bd102800d4860b35
jenniferneale <jeneale6@gmail.com>
2017-11-08 12:11:06 -0500

    Put secrets on env vars

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -1,10 +1,16 @@
 const configureStripe = require('stripe');
-const { PK_TEST_SECRET_STRIPE } = require('../../secrets.js');
-const { PK_LIVE_SECRET_STRIPE } = require('../../secrets.js');
+//const { PK_LIVE_SECRET_STRIPE } = require('../../secrets.js');
 
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
+/*if (process.env.NODE_ENV === 'development') {
+  const { PK_TEST_SECRET_STRIPE } = require('../../secrets.js');
+
+}*/
+
+const STRIPE_SECRET_KEY = process.env.PK_TEST_SECRET_STRIPE || "";
+
+/*process.env.NODE_ENV === 'production'
   ? PK_LIVE_SECRET_STRIPE 
-  : PK_TEST_SECRET_STRIPE;
+  : PK_TEST_SECRET_STRIPE;*/
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
