1f20b7bfc355252403d6b9581885260a12a0a1f1
jenniferneale <jeneale6@gmail.com>
2017-11-07 21:00:20 -0500

    updated package.json seed path

--- script/seed.js
+++ script/seed.js
@@ -1,51 +1,72 @@
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
-const db = require('../server/db')
-const {User} = require('../server/db/models')
+'use strict'
+var Promise = require("bluebird");
+var {
+  User,
+  Teachables,
+  Transactions,
+  Orders
+} = require('./models');
+var db = require('./index');
+var data = require('./data');
 
 async function seed () {
-  await db.sync({force: true})
-  console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
-
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+  await db.sync({ force: true })
+  const users = toObject(await Promise.map(data.user, (data => User.create(data))))
+  const teachables = toObject(await Promise.map(data.teachables, (data => Teachables.create(data))))
+  const orders = toObject(await Promise.map(data.orders, (data => Orders.create(data))))
+  const transactions = toObject(await Promise.map(data.transactions, (data => Transactions.create(data))))
+  const {'Annelise Thorsen': Annelise,'Ella Pitassi': Ella, 'Jennifer Neale': Jennifer, 'Guang Zhu': Guang, 'Anule Ndukwu': Anule, 'April Rueb': April, 'Arianna Lanz': Arianna, Ashi, 'Emily Intersimone': EmilyI, 'Blanca Sanchez': Blanca, 'Cara Lang': Cara, 'Caryn McCarthy': Caryn, 'Cecilia Chang': Cecilia, 'Christina Ng': Christina, 'Evlis Henry': Evlis, 'Emily Jordan': EmilyJ, 'Erica Chai': Erica, 'Joyce Ren': Joyce, 'Keri Miller': Keri, 'Layla Hedges': Layla, 'Lina Morales': Lina, 'Maria Betances': Maria, 'Moyouri Bhattacharjee': Momo, 'Renee Sajedian': Renee, 'Samantha Zhang': Sam, 'Shannon Kendall': Shannon, 'Shelby Rackley': Shelby, 'Shiratie Prodhan': Shiratie, 'Priya Vaidyanath': Priya, 'Eleni Arvanitis': Eleni} = users
+  const {React, Redux, Express, Sequelize, Crafting, Knitting, Writing, Reading, Numbers, Eating, Gaming, FullStack, ReactRedux, Feminism} = teachables
+  return Promise.all([
+    Ashi.setTeachables([FullStack, Writing, Feminism])
+  , Ella.setTeachables([Express, Eating, Feminism])
+  , Annelise.setTeachables([Numbers, Writing, Redux, Feminism])
+  , Jennifer.setTeachables([FullStack, Gaming, Feminism])
+  , Guang.setTeachables([Express, React, Feminism])
+  , Anule.setTeachables([Knitting, React, Sequelize, Feminism])
+  , April.setTeachables([Crafting, Writing, ReactRedux, Feminism])
+  , Arianna.setTeachables([Reading, React, Feminism])
+  , EmilyI.setTeachables([FullStack, Feminism])
+  , Blanca.setTeachables([Reading, React, Redux, Feminism])
+  , Cara.setTeachables([Reading, Sequelize, Feminism])
+  , Caryn.setTeachables([ReactRedux, Knitting, Reading, Feminism])
+  , Cecilia.setTeachables([Eating, ReactRedux, Feminism])
+  , Christina.setTeachables([Express, Reading, Feminism])
+  , Evlis.setTeachables([ReactRedux, Numbers, Feminism])
+  , EmilyJ.setTeachables([Express, Writing, Reading, Feminism])
+  , Erica.setTeachables([FullStack, Feminism])
+  , Joyce.setTeachables([Gaming, Crafting, Express, Feminism])
+  , Keri.setTeachables([FullStack, Crafting, Feminism])
+  , Layla.setTeachables([Writing, React, Reading, Feminism])
+  , Lina.setTeachables([Numbers, FullStack, Feminism])
+  , Maria.setTeachables([FullStack, Feminism])
+  , Momo.setTeachables([Gaming, Sequelize, Feminism])
+  , Renee.setTeachables([Reading, ReactRedux, Feminism])
+  , Sam.setTeachables([Redux, Reading, Writing, Feminism])
+  , Shannon.setTeachables([React, Redux, Feminism])
+  , Shelby.setTeachables([Sequelize, React, Feminism])
+  , Shiratie.setTeachables([Sequelize, Reading, Feminism])
+  , Priya.setTeachables([FullStack, Feminism])
+  , Eleni.setTeachables([React, Redux, Feminism])
   ])
-  // Wowzers! We can even `await` on the right-hand side of the assignment operator
-  // and store the result that the promise resolves to in a variable! This is nice!
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
 }
 
-// Execute the `seed` function
-// `Async` functions always return a promise, so we can use `catch` to handle any errors
-// that might occur inside of `seed`
-seed()
-  .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
-  })
-  .then(() => {
-    console.log('closing db connection')
-    db.close()
-    console.log('db connection closed')
-  })
+Promise.resolve(seed())
+.then(function () {
+  console.log("Finished inserting data");
+})
+.catch(function (err) {
+  console.error("There was totally a problem", err, err.stack);
+})
+.finally(function () {
+  db.close();
+  console.log("connection closed");
+  return null;
+});
 
-/*
- * note: everything outside of the async function is totally synchronous
- * The console.log below will occur before any of the logs that occur inside
- * of the async function
- */
-console.log('seeding...')
+function toObject (array) {
+  return array.reduce(function(all, x){
+    all[x.name] = x;
+    return all
+  }, {})
+}
