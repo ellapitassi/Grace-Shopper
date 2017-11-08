c88c16372928e13c8083700bab2e83e5ffdf1963
jenniferneale <jeneale6@gmail.com>
2017-11-08 11:05:54 -0500

    stripefix

--- client/constants/stripe.js
+++ client/constants/stripe.js
@@ -1,5 +1,5 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
+/*const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
   ? PK_LIVE_STRIPE //Replace with Live key
-  : 'pk_test_Vdaphjjy2tZY9I6WDfm5Bej9';
+  : 'pk_test_Vdaphjjy2tZY9I6WDfm5Bej9';*/
 
-export default STRIPE_PUBLISHABLE;
+export default process.env.STRIPE_PUBLISHABLE || 'pk_test_Vdaphjjy2tZY9I6WDfm5Bej9';
